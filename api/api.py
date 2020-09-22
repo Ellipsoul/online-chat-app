f96f96712f4c7d5eeddda772df3690b08622ca67
Aron Teh <ewt17@ic.ac.uk>
2020-09-22 22:53:59 +0800

    Small cleanup

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import time
+import json
 from flask import Flask, render_template, request, jsonify
 from flask_restful import Api, Resource
 
@@ -7,38 +8,42 @@ app = Flask(__name__)
 api = Api(app)
 print("Backend is now running")
 
+# TODO: Will need to be converted into a databse later
 messages = []
 
-# Root directory
+
+# Root directory (likely not needed)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+# Gets the current time (Not required for chat app)
 class getTime(Resource):
     def get(self):
         return {'time':time.time()}
 api.add_resource(getTime, "/time")
 
+
+# POST method to accept HTTP POST request from client, sending a message
 class send_message(Resource):
     def post(self):
-        print('Reached post')
-        message_data = request.json
-        messages.append(message_data)
-        print(f"Last message sent: {message_data}")
-        print(f"All messages so far are {messages}")
+        message_data = request.json     # Retrieve message JSON data from client
+        messages.append(message_data)   # Append message to all messages
+
+        message_formatted = json.dumps(message_data, indent=2)
+        messages_formatted = json.dumps(messages, indent=2)
+        print(f"\nLast message sent:\n {message_formatted} \n")
+        print(f"All messages so far:\n {messages_formatted}")
+
         return
 api.add_resource(send_message, "/send_message")
+
 
 class get_messages(Resource):
     def get(self):
         return {"messages": messages}
 api.add_resource(get_messages, "/get_messages")
-
-@app.route('/api', methods=['GET', 'POST'])
-def get_api():
-    info = request.json
-    print(info)
-    return info
 
 # Is this necessary for backend?
 if __name__ == "__main__":
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, SyntheticEvent} from 'react';
 import './App.css';
 
 function App() {
@@ -13,46 +13,64 @@ function App() {
     });
   }, []);
 
+  // Sets the user's name
+  const [name, setName] = useState("Anonymous");
+  const handleSubmitName = (e:SyntheticEvent) => {
+    e.preventDefault()
+    console.log("Name changed!")
+  }
+
   // Send information about input field to backend
   const [messageField, setMessageField] = useState("");
-  const handleSubmit = () => {
+  const handleSubmitMessage = (e:SyntheticEvent) => {
+    e.preventDefault() // Prevents page from reloading after submitting message
+    console.log("Message sent!")
     var d = new Date(Date.now());
     fetch("/send_message", {
       method:"POST",
       headers: {"content_type":"application/json"},
       body:JSON.stringify(
-        { "message": messageField,
-          "date": d.toString()
+        { 
+          "name": name,
+          "date": d.toString(),
+          "message": messageField,
         })
       })
     .then(response => { return response.json()})
-  } 
+    return false
+  }
 
+  // Retrieve all current messages from the server
+  const [currentMessages, setCurrentMessages] = useState("");
   function show_messages() {
     fetch("/get_messages")
     .then((res) => res.json())
-    .then((data) => {console.log(data)})
+    .then((data) => {
+      setCurrentMessages(JSON.stringify(data, null ,4))
+    })
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
         <p> The current time is { currentTime } </p>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmitName}>
+          <label> 
+            Type your name here:
+            <input 
+              type="text"
+              id="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Submit Name" />
+        </form>
+
+
+        <form onSubmit={handleSubmitMessage}>
           <label>
             Type your message here:
             <input
@@ -60,11 +78,12 @@ function App() {
               value={messageField}
               onChange={e => setMessageField(e.target.value)}
             />
-            </label>
-          <input type="submit" value="Submit" />
+          </label>
+          <input type="submit" value="Send Message" />
         </form>
 
         <button onClick={show_messages}> Show all messages </button>
+        <p> { currentMessages } </p>
 
       </header>
     </div>
