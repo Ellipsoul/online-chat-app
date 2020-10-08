e3e5ea787037139b9e86b6630f63a7d29c133c41
Aron Teh <ewt17@ic.ac.uk>
2020-10-08 15:06:59 +0800

    Just before converting message storage functionality to SQLite

--- api/api.py
+++ api/api.py
@@ -3,11 +3,25 @@ import time
 
 from flask import Flask, jsonify, render_template, request
 from flask_restful import Api, Resource
+import sqlite3
 
 # Initialise app
 app = Flask(__name__)
 api = Api(app)
+# Sqlite Connection and database creation
+conn = sqlite3.connect('messages.db')
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS messages (
+            name text,
+            date text,
+            message text
+            )""")
+# c.execute("""DELETE FROM messages""")
+conn.commit()
+conn.close()
+
 print("Backend is now running")
+
 
 # TODO: Will need to be converted into a databse later
 messages = []
@@ -22,29 +36,47 @@ def index():
 class send_message(Resource):
     def post(self):
         message_data = request.json     # Retrieve message JSON data from client
-        messages.append(message_data)   # Append message to all messages
 
-		# Logging messages to the console
+        conn = sqlite3.connect('messages.db')
+        c = conn.cursor()
+        # Set up message insertion and execute
+        sqlite_insert = """ INSERT INTO messages 
+                        (name, date, message)
+                        VALUES (?, ?, ?); """
+        data_tuple = (message_data['name'], message_data['date'], message_data['message'])
+        c.execute(sqlite_insert, data_tuple)
+        conn.commit()
+        conn.close()
+
+        messages.append(message_data)   # Append message to all messages
+        
+        # Logging messages to the console
         # message_formatted = json.dumps(message_data, indent=2)
         # messages_formatted = json.dumps(messages, indent=2)
         # print(f"\nLast message sent:\n {message_formatted} \n")
         # print(f"All messages so far:\n {messages_formatted}")
-
         return
 api.add_resource(send_message, "/send_message")
 
 
 class get_all_messages(Resource):
     def get(self):
+        conn = sqlite3.connect('messages.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM messages")
+        all_messages = c.fetchall()
+        print(all_messages)
+        conn.commit()
+        conn.close()
         return {"messages": messages}
 api.add_resource(get_all_messages, "/get_all_messages")
 
 class clear_messages(Resource):
-	def delete(self):
-		global messages
-		messages = []
-		print("Messages cleared!")
-		return
+    def delete(self):
+        global messages
+        messages = []
+        print("Messages cleared!")
+        return
 api.add_resource(clear_messages, "/clear_messages")
 
 # Is this necessary for backend?
