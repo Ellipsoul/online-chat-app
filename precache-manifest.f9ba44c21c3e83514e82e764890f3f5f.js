e790087e44bb34379f9cf0532fa90ee3ddd094f8
Aron Teh <ewt17@ic.ac.uk>
2020-10-23 19:56:51 +0100

    Updates

---
+++ precache-manifest.f9ba44c21c3e83514e82e764890f3f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c63f4219ee787adb2cfbdb42d3d321",
+    "url": "/online-chat-app/index.html"
+  },
+  {
+    "revision": "5e7cd642cc8031156dd7",
+    "url": "/online-chat-app/static/css/main.e9eaacef.chunk.css"
+  },
+  {
+    "revision": "f85959c83b40331aa583",
+    "url": "/online-chat-app/static/js/2.f20aea5f.chunk.js"
+  },
+  {
+    "revision": "448282f969604208dc19c307c6a74f1d",
+    "url": "/online-chat-app/static/js/2.f20aea5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7cd642cc8031156dd7",
+    "url": "/online-chat-app/static/js/main.9f562621.chunk.js"
+  },
+  {
+    "revision": "a9cba39278677405c0da",
+    "url": "/online-chat-app/static/js/runtime-main.ecd1c244.js"
+  }
+]);
