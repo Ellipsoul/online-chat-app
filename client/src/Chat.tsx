5249f6f6c894458faf993f980c43899fbfeb80c8
Aron Teh <ewt17@ic.ac.uk>
2020-10-24 11:15:22 +0100

    Removed fetch POST request

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -47,28 +47,8 @@ export default function Chat(props: chatProps) {
 				date: date.toString(),
 				message: message,
 				date_unix: date_unix
-			})
-			.then(() => {
-				console.log(`Message sent: ${message}`)
-			}, (error) => {
-				console.log(error)
-			})
-
-
-			// fetch("https://online-chat-app-ellipsoul.herokuapp.com/api/send_message", {
-			// method:"POST",
-			// mode:"cors",
-			// headers: {"content_type":"application/json"},
-			// body:JSON.stringify(
-			// 	{ 
-			// 	"name": name,
-			// 	"date": date.toString(),
-			// 	"message": message,
-			// 	"date_unix": date_unix                     // Unix timestamp
-			// 	})
-			// })
-			// .then(response => { return response.json()})
-			// console.log("Message sent!")
+			});
+			console.log(`Message sent: ${message}`);
 
 			setMessageField("")                            // Empty the message container
 			// Increment message key and add message locally
